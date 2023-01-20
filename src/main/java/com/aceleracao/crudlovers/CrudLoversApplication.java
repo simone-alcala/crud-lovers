12e1f428160a766f77bab643bb4e5909a74a6146
simone.alcala <simone.alcala@gmail.com>
2023-01-20 13:15:48 -0300

    Chore: First commit

---
+++ src/main/java/com/aceleracao/crudlovers/CrudLoversApplication.java
@@ -0,0 +1,13 @@
+package com.aceleracao.crudlovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudLoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudLoversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aceleracao/crudlovers/CrudLoversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceleracao.crudlovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
